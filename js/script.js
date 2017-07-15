f68399cfb638b91f0eccd0ed92295b3fbed726e4
Omayeli Arenyeka <aomayeli@gmail.com>
2017-07-15 12:23:44 -0700

    finalizing list

--- js/script.js
+++ js/script.js
@@ -264,4 +264,5 @@ function calculateDownloadTimes(wifiSpeeds, sizeToLoad) {
 	  }
 	}
 }
-
+/* griffin byron code **/
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
